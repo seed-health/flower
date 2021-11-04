19f4fefcd7b72d196893d2aef7475ef7950bcf21
Alexander Lang <alex@seed.com>
2021-11-04 11:35:00 -0700

    Add seed login handler

--- flower/views/auth.py
+++ flower/views/auth.py
@@ -340,3 +340,72 @@ class OktaLoginHandler(BaseHandler, tornado.auth.OAuth2Mixin):
         if self.application.options.url_prefix and next_[0] != '/':
             next_ = '/' + next_
         self.redirect(next_)
+
+class SeedLoginHandler(BaseHandler, tornado.auth.OAuth2Mixin):
+
+    _OAUTH_NO_CALLBACKS = False
+
+    @property
+    def base_url(self):
+        return "http://172.18.0.1:8000"
+        return os.environ.get('FLOWER_OAUTH2_SEED_BASE_URL')
+
+    @property
+    def _OAUTH_AUTHORIZE_URL(self):
+        return F"{self.base_url}/o/authorize"
+
+    @property
+    def _OAUTH_ACCESS_TOKEN_URL(self):
+        return F"{self.base_url}/o/token"
+
+    @tornado.gen.coroutine
+    def get_access_token(self, redirect_uri, code):
+        body = urlencode({
+            "redirect_uri": redirect_uri,
+            "code": code,
+            "client_id": self.settings[self._OAUTH_SETTINGS_KEY]['key'],
+            "client_secret": self.settings[self._OAUTH_SETTINGS_KEY]['secret'],
+            "grant_type": "authorization_code",
+        })
+
+        response = yield self.get_auth_http_client().fetch(
+            self._OAUTH_ACCESS_TOKEN_URL,
+            method="POST",
+            headers={'Content-Type': 'application/x-www-form-urlencoded',
+                     'Accept': 'application/json'}, body=body)
+
+        if response.error:
+            raise tornado.auth.AuthError(
+                'OAuth authenticator error: %s' % str(response))
+
+        raise tornado.gen.Return(json.loads(response.body.decode('utf-8')))
+
+    @tornado.gen.coroutine
+    def get(self):
+        redirect_uri = self.settings['oauth']['redirect_uri']
+        if self.get_argument('code', False):
+            token = yield self.get_ar(
+                redirect_uri=redirect_uri,
+                code=self.get_argument('code'),
+            )
+            yield self._on_auth(user)
+        else:
+            yield self.authorize_redirect(
+                redirect_uri=redirect_uri,
+                client_id=self.settings['oauth']['key'],
+                response_type='code',
+            )
+
+    @tornado.gen.coroutine
+    def _on_auth(self, access_token_response):
+        if not access_token_response:
+            raise tornado.web.HTTPError(500, 'OAuth authentication failed')
+        access_token = access_token_response['access_token']
+        print(access_token)
+        # We need to verify Token to get an email?
+        #self.set_secure_cookie("user", str(email))
+        self.set_secure_cookie("user", str(email))
+        next_ = self.get_argument('next', self.application.options.url_prefix or '/')
+        if self.application.options.url_prefix and next_[0] != '/':
+            next_ = '/' + next_
+        self.redirect(next_)
